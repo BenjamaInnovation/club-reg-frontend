c8fd36628a4e9b58ed4d92d8bea640bf0b2f02ef
Ratchanon Panmas <qq.panmas@gmail.com>
2022-05-12 21:15:44 +0700

    ✨  Feat: lottie animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Player } from "@lottiefiles/react-lottie-player";
 import type { NextPage } from "next";
 import Countdown from "../components/partial/Countdown";
 import Navbar from "../components/partial/Navbar";
@@ -7,16 +8,27 @@ const Home: NextPage = () => {
     <>
       <Navbar />
       <div className="container mx-auto">
-        <header className="p-6 flex flex-col items-center text-center intro">
-          <h1 className="text-9xl">
-            <span className="text-5xl text-neutral">ระบบลงทะเบียน</span>
-            <br />
-            <span className="font-bold text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text">
-              กิจกรรมพัฒนาผู้เรียน
-            </span>
-          </h1>
-          <h2 className="text-3xl text-red-500">โรงเรียนเบญจมราชูทิศ</h2>
-          <Countdown />
+        <header className="p-6 flex items-center intro justify-between">
+          <div className="flex flex-col items-start">
+            <h1 className="text-7xl">
+              <span className="text-5xl text-neutral">ระบบลงทะเบียน</span>
+              <br />
+              <span className="font-bold text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text">
+                กิจกรรมพัฒนาผู้เรียน
+              </span>
+            </h1>
+            <h2 className="text-3xl text-red-500">โรงเรียนเบญจมราชูทิศ</h2>
+            <Countdown />
+          </div>
+          <Player
+            autoplay
+            loop
+            src="https://assets1.lottiefiles.com/packages/lf20_sknsem5j.json"
+            style={{
+              height: "400px",
+              width: "400px",
+            }}
+          />
         </header>
       </div>
     </>
